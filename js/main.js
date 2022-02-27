e57c2b0f6f46f959df046bbee90f5065b51affe0
Biniam Nega <biniamnega29@gmail.com>
2022-02-27 23:23:35 +0000

    Added eventlisteners that changes the value of the buttons when clicked

--- js/main.js
+++ js/main.js
@@ -1,12 +1,48 @@
 $(document).ready(function() {
 
+    // A function to add event handler to all the buttons
+    function addListeners(givens) {
+        var counter = 0;
+        for(var i = 0; i < 81; i++) {
+            if(counter === i) {
+                counter++;
+                continue;
+            }
+            (function() {
+                $('#strim-btn-' + i).click(function() {
+                    var value = $(this).val() === ''? 1: parseInt($(this).val()) + 1;
+                    if(value === 7) {
+                        value = '';
+                    }
+                    $(this).val(value);
+                    checkRow(this);
+                    checkColumn(this);
+                    checkSquare(this);
+                    checkFinished();
+                });
+            }());
+        }
+    }
+
+    // A function that checks if there is a duplicate in a row
+    function checkRow(button) {
+
+    }
+
+    // A function that checks if there is a duplicate in a column
+    function checkColumn(button) {}
+
+    // A function that checks if there is a duplicate in the 3x3 square
+    function checkSquare(button) {}
+
+    // A function that checks whether the game is finished or not
+    function checkFinished(button) {}
+
     // A constructor for buttons
     function Button(value, index) {
-        this.value = value;
-        this.index = index;
         this.render = function() {
             var bg = value === '' ? 'white': 'gray w3-hover-gray';
-            return '<input type="button" class="w3-button w3-cicle w3-border w3-' + bg + '" value="' + value + '" id="strim-btn-' + this.index + '"/>';
+            return '<input type="button" class="w3-button w3-cicle w3-border w3-' + bg + '" value="' + value + '" id="strim-btn-' + index + '"/>';
         }
     }
 
@@ -19,12 +55,15 @@ $(document).ready(function() {
                     $('#game-area').html($('#game-area').html() + '<br/>');
                 }
                 if(i === givens[counter]) {
-                    $('#game-area').html($('#game-area').html() + new Button(values[counter], i).render());
+                    var button = new Button(values[counter], i)
+                    $('#game-area').html($('#game-area').html() + button.render());
                     counter++;
                     continue;
                 }
-                $('#game-area').html($('#game-area').html() + new Button('', i).render());
+                var button = new Button('', i)
+                $('#game-area').html($('#game-area').html() + button.render());
             }
+            addListeners(givens);
         }
     }
 
