4fe8ccf6279af72d8463a18c58b3b7ae3ac1da29
Biniam Nega <biniamnega29@gmail.com>
2022-02-28 17:06:44 +0000

    The buttons change red when their value is duplicated in a row, column or square

--- js/main.js
+++ js/main.js
@@ -1,39 +1,81 @@
 $(document).ready(function() {
 
+    // Add some functionalities to the Array prototype
+    my_lib.augmentArray();
+
+    var duplicates = [];
+
+    var rows = [[], [], [], [], [], [], [], [], []];
+    var columns = [[], [], [], [], [], [], [], [], []];
+    var squares = [[1, 2, 3, 10, 11, 12, 19, 20, 21], [4, 5, 6, 13, 14, 15, 22, 23, 24],
+    [7, 8, 9, 16, 17, 18, 25, 26, 27], [28, 29, 30, 37, 38, 39, 46, 47, 48],
+    [31, 32, 33, 40, 41, 42, 49, 50, 51], [34, 35, 36, 43, 44, 45, 52, 53, 54], 
+    [55, 56, 57, 64, 65, 66, 73, 74, 75], [58, 59, 60, 67, 68, 69, 76, 77, 78], 
+    [61, 62, 63, 70, 71, 72, 79, 80, 81]];
+    for(var i = 1; i <= 81; i++) {
+        rows[parseInt((i - 1) / 9)].push(i);
+        columns[parseInt((i - 1) % 9)].push(i);
+    }
+
     // A function to add event handler to all the buttons
     function addListeners(givens) {
         var counter = 0;
-        for(var i = 0; i < 81; i++) {
-            if(counter === i) {
+        for(var i = 0; i <= 81; i++) {
+            if(givens[counter] === i) {
                 counter++;
                 continue;
             }
             (function() {
+                var c = i;
                 $('#strim-btn-' + i).click(function() {
                     var value = $(this).val() === ''? 1: parseInt($(this).val()) + 1;
-                    if(value === 7) {
+                    if(value === 10) {
                         value = '';
                     }
                     $(this).val(value);
-                    checkRow(this);
-                    checkColumn(this);
-                    checkSquare(this);
+                    check(c, 'row');
+                    check(c, 'column');
+                    check(c, 'square');
                     checkFinished();
                 });
             }());
         }
     }
 
-    // A function that checks if there is a duplicate in a row
-    function checkRow(button) {
-
+    function changeBg(btn1, btn2) {
+        btn1.css('background-color', 'red');
+        btn2.css('background-color', 'red');
     }
 
-    // A function that checks if there is a duplicate in a column
-    function checkColumn(button) {}
-
-    // A function that checks if there is a duplicate in the 3x3 square
-    function checkSquare(button) {}
+    
+    function check(index, what) {
+        switch(what) {
+            case 'row':
+                var ar = rows;
+                break;
+            case 'column':
+                var ar = columns;
+                break;
+            default:
+                var ar = squares;
+                break;
+        }
+        var button = $('#strim-btn-' + index);
+        for(var i = 0; i < ar.length; i++) {
+            if(ar[i].inArray(index)) {
+                for(var j = 0; j < ar[i].length; j++) {
+                    if(index === ar[i][j]) {
+                        continue;
+                    }
+                    var secButton = $('#strim-btn-' + ar[i][j]);
+                    if(button.val() === secButton.val()) {
+                        changeBg(button, secButton);
+                    }
+                }
+                break;
+            }
+        }
+    }
 
     // A function that checks whether the game is finished or not
     function checkFinished(button) {}
@@ -41,8 +83,8 @@ $(document).ready(function() {
     // A constructor for buttons
     function Button(value, index) {
         this.render = function() {
-            var bg = value === '' ? 'white': 'gray w3-hover-gray';
-            return '<input type="button" class="w3-button w3-cicle w3-border w3-' + bg + '" value="' + value + '" id="strim-btn-' + index + '"/>';
+            var bg = value === '' ? 'white': 'darkgray';
+            return '<input type="button" class="w3-button w3-cicle w3-border " value="' + value + '" id="strim-btn-' + index + '" style="background-color:' + bg + '"/>';
         }
     }
 
